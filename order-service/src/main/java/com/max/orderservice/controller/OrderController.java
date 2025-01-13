64a9aa59226dbc65b9e2545baabc7f28eba7a710
Max <kalmro4@gmail.com>
2025-01-13 15:49:26 +0700

    initialize base, email, order, and stock services with application properties, test classes, and Kafka configuration

---
+++ order-service/src/main/java/com/max/orderservice/controller/OrderController.java
@@ -0,0 +1,36 @@
+package com.max.orderservice.controller;
+
+import com.max.basedomains.dto.Order;
+import com.max.basedomains.dto.OrderEvent;
+import com.max.orderservice.kafka.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    @Autowired
+    private OrderProducer orderProducer;
+
+    //http://localhost:8080/api/v1/orders
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order){
+
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("order status is in pending state");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order placed successfully ...";
+    }
+}
